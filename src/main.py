cb4b39b50bf8ae5a4a18afd1d7142538b5e5bdaa
Lizh <4a5g0030@stust.edu.tw>
2019-11-11 03:35:16 +0800

    PATH AND ALL IMAGE
    Set any path and use glob.glob, can read any .bmp in imagedir

--- src/main.py
+++ src/main.py
@@ -1,29 +1,45 @@
 import OrganImageReader as oir
 import cv2
+import os
+import glob
 
 # 使否顯示除錯訊息
 debug = True
 
 
 def main():
+    # 各種路徑
+    ROOT_DIR = os.path.abspath("../")
+    IMAGES_DIR = os.path.join(ROOT_DIR, "resource/(VKH) Segmented Images (1,000 X 570)/")
+    JSON_DIR = os.path.join(ROOT_DIR, "json")
+    TABLE_PATH = os.path.join(ROOT_DIR, "resource/color.txt")
+
+    # 開json檔
+    json = open(JSON_DIR + "/output.json", 'w')
+    json.write("{ \n")
+
     organ_reader = oir.OrganImageReader(debug)
     # 讀取資料表
-    organ_reader.load_table('../resource/color.txt')
-    # 讀取圖片
-    organ_reader.load_image('../resource/BODY/(VKH) Segmented Images (1,000 X 570)/1570.bmp')
-    # 顯示原始圖片
-    cv2.imshow("Origin Image", organ_reader.image_origin)
-    # 找出圖片的器官
-    organ_reader.find_organ()
-    # 建立過濾後器官圖片
-    organ_reader.filter_organ(297)
-    # 顯示過濾後圖片
-    cv2.imshow("Filter Image", organ_reader.image_filter)
-    # 建立過濾後圖片輪廓
-    image = organ_reader.draw_contours()
-    # 顯示繪製完輪廓的圖片
-    if image is not None:
-        cv2.imshow('Contours', image)
+    organ_reader.load_table(TABLE_PATH)
+    # 讀取整個資料夾的.bmp
+    images = glob.glob(IMAGES_DIR + "*.bmp")
+    # 逐張
+    for image in images:
+        # 讀取圖片
+        organ_reader.load_image(image)
+        # 顯示原始圖片
+        cv2.imshow("Origin Image", organ_reader.image_origin)
+        # 找出圖片的器官
+        organ_reader.find_organ()
+        # 建立過濾後器官圖片
+        organ_reader.filter_organ(297)
+        # 顯示過濾後圖片
+        cv2.imshow("Filter Image", organ_reader.image_filter)
+        # 建立過濾後圖片輪廓
+        image = organ_reader.draw_contours()
+        # 顯示繪製完輪廓的圖片
+        if image is not None:
+            cv2.imshow('Contours', image)
 
 
 if __name__ == '__main__':
